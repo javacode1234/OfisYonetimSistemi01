19231b80d3fca4fbcb008b45265bf882b8da0d82
javacode1234 <uzunmumy@gmail.com>
2024-06-26 20:11:18 +0300

    Anasayfa İletişim kısmında bazı değişiklikler yapıldı.

--- src/main/java/com/ofisyonetimsistemi/controllers/HomeController.java
+++ src/main/java/com/ofisyonetimsistemi/controllers/HomeController.java
@@ -15,27 +15,7 @@ import com.ofisyonetimsistemi.models.User;
 public class HomeController {
 	
 	
-	@GetMapping("/index1")
-	public String getHomePage1(Model model) {
-		model.addAttribute("title","SMMM Ofis Yönetim Sistemi.");
-		model.addAttribute("ilkMesaj","Yeni Proje ilk mesaj!!!");
 		
-		List<User> userList = new ArrayList<>();
-		
-		User user1 = new User(1,"Muammer","uzun","mumyuzun@hotmail.com","muammer","pwd1");
-		User user2 = new User(2,"Muammer","uzun","mumyuzun@hotmail.com","muammer","pwd2");
-		User user3 = new User(3,"Muammer","uzun","mumyuzun@hotmail.com","muammer","pwd3");
-		User user4 = new User(4,"Muammer","uzun","mumyuzun@hotmail.com","muammer","pwd4");
-		
-		userList.add(user1);
-		userList.add(user2);
-		userList.add(user3);
-		userList.add(user4);
-			
-		model.addAttribute("users",userList);
-		return "index1";
-	}
-	
 	@GetMapping("/")
 	public String getHomePage(Model model) {
 		model.addAttribute("title","SMMM Muammer UZUN");
@@ -43,6 +23,11 @@ public class HomeController {
 		model.addAttribute("anaSayfaBaslik","Serbest Muhasebeci Mali Müşavir Ofis Yönetim Sistemi.");
 		model.addAttribute("anaSayfaAltBaslik","Nerede olursanız olun müşterilerinizin bilgilerine ulaşın. Müşteriler de kendi durumlarını görsün.");
 		model.addAttribute("videoLink","https://www.youtube.com/watch?v=tuus0A9u2jo");
+		model.addAttribute("adres","Yakuplu Mah. 100. Sk. No:30/B Beylikdüzü / İstanbul");
+		model.addAttribute("telefon","538 332 63 34");
+		model.addAttribute("email","mumyuzun@hotmail.com");
+		model.addAttribute("konum","https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d752.876889483895!2d28.670593283877572!3d40.99226275881076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14b55f89aa23eed9%3A0xcd20d5bf724b110b!2sYakuplu%2C%20100.%20Sk.%2030%20B%2C%2034524%20Beylikd%C3%BCz%C3%BC%2F%C4%B0stanbul!5e0!3m2!1str!2str!4v1719420695704!5m2!1str!2str");
+		
 		return "index";
 		
 	}
