25b33d65c08aa25e50ea4cae3abf4229890f3e7d
javacode1234 <uzunmumy@gmail.com>
2024-06-29 23:07:59 +0300

    Profile sayfası çalışıldı.

--- src/main/java/com/ofisyonetimsistemi/controllers/AdminPanelController.java
+++ src/main/java/com/ofisyonetimsistemi/controllers/AdminPanelController.java
@@ -11,14 +11,56 @@ public class AdminPanelController {
 	
 	@GetMapping("/admin-panel")
 	public String getAdminPanel(Model model) {
+		model.addAttribute("dashboardtitle","SMMM Muammer UZUN");
 		model.addAttribute("smmmisim","Muammer UZUN");
 		return "adminpanel/index";
 	}
 	
 	@GetMapping("/components-alerts")
 	public String getAlerts(Model model) {
-		
+		model.addAttribute("dashboardtitle","SMMM Muammer UZUN");
+		model.addAttribute("smmmisim","Muammer UZUN");
 		return "adminpanel/components-alerts";
+	}
+	
+	@GetMapping("/user-profile")
+	public String getUserProfile(Model model) {
+		model.addAttribute("dashboardtitle","SMMM Muammer UZUN");
+		model.addAttribute("smmmisim","Muammer UZUN");
+		model.addAttribute("tabpane","view");
+		return "adminpanel/users-profile";
+	}
+	
+	@GetMapping("/user-profile-view")
+	public String getUserProfileWiew(Model model){
+		model.addAttribute("dashboardtitle","SMMM Muammer UZUN");
+		model.addAttribute("smmmisim","Muammer UZUN");
+		model.addAttribute("tabpane","view");
+		return "adminpanel/users-profile";
+	}
+	
+	@GetMapping("/user-profile-edit")
+	public String getUserProfileEdit(Model model){
+		model.addAttribute("dashboardtitle","SMMM Muammer UZUN");
+		model.addAttribute("smmmisim","Muammer UZUN");
+		model.addAttribute("tabpane","edit");
+		return "adminpanel/users-profile";
+	}
+	
+	@GetMapping("/user-profile-settings")
+	public String getUserProfileSettings(Model model){
+		model.addAttribute("dashboardtitle","SMMM Muammer UZUN");
+		model.addAttribute("smmmisim","Muammer UZUN");
+		model.addAttribute("tabpane","settings");
+		return "adminpanel/users-profile";
+	}
+	
+	@GetMapping("/user-profile-changepwd")
+	public String getUserProfileChangepwd(Model model){
+		model.addAttribute("dashboardtitle","SMMM Muammer UZUN");
+		model.addAttribute("smmmisim","Muammer UZUN");
+		model.addAttribute("tabpane","changepwd");
+		return "adminpanel/users-profile";
 	}
 
 }
