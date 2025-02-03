462c2b795843f5ca8fb976ffcbf9b346dd1c6f63
javacode1234 <uzunmumy@gmail.com>
2025-02-03 20:02:17 +0300

    railway deneme 1

--- src/main/java/com/ofisyonetimsistemi/security/securityconfig/SecurityConfig.java
+++ src/main/java/com/ofisyonetimsistemi/security/securityconfig/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
 		
 		.authorizeHttpRequests(req->{
 			req.requestMatchers("/arshahomepage/**", "/niceadminpanel/**", "/customjs/**", "/customcss/**", "/loginimages/**").permitAll();//resources//static
-			req.requestMatchers("/", "/adminpanel/login", "/portfolio-details/**", "/service-details/**").permitAll();
+			req.requestMatchers("/", "/login", "/portfolio-details/**", "/service-details/**").permitAll();
 			req.requestMatchers("/user-register-request/**", "/send-message/**", "/add-to-mail-list/**").permitAll();
 			
 			req.requestMatchers("/api/v1/**").hasRole("ADMIN");
