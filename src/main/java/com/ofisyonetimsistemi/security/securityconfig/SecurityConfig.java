f3d87a7defcd864f34f48a631212e6c696985a34
javacode1234 <uzunmumy@gmail.com>
2025-02-04 20:28:28 +0300

    login problem

--- src/main/java/com/ofisyonetimsistemi/security/securityconfig/SecurityConfig.java
+++ src/main/java/com/ofisyonetimsistemi/security/securityconfig/SecurityConfig.java
@@ -47,8 +47,8 @@ public class SecurityConfig {
 		
 		.authorizeHttpRequests(req->{
 			req.requestMatchers("/arshahomepage/**", "/niceadminpanel/**", "/customjs/**", "/customcss/**", "/loginimages/**").permitAll();//resources//static
-			req.requestMatchers("/", "/error/**", "/adminpanel/login", "/portfolio-details/**", "/service-details/**").permitAll();
-			req.requestMatchers("/user-register-request/**", "/send-message/**", "/add-to-mail-list/**").permitAll();
+			req.requestMatchers("/", "/error/**", "/adminpanel/login/**", "/portfolio-details/**", "/service-details/**").permitAll();
+			req.requestMatchers("/user-register-request/**", "/send-message/**", "/add-to-mail-list/**","/login").permitAll();
 			
 			req.requestMatchers("/api/v1/**").hasRole("ADMIN");
 			
