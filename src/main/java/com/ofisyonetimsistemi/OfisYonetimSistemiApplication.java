0f9ffb4f9defdde141060dd04ffef651e7da418d
javacode1234 <uzunmumy@gmail.com>
2025-02-05 14:13:17 +0300

    login page white label error

--- src/main/java/com/ofisyonetimsistemi/OfisYonetimSistemiApplication.java
+++ src/main/java/com/ofisyonetimsistemi/OfisYonetimSistemiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = "com.ofisyonetimsistemi.*")
+@ComponentScan(basePackages = "com.ofisyonetimsistemi.*")
 public class OfisYonetimSistemiApplication {
 
 	public static void main(String[] args) {
